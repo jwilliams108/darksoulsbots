5592eefa6b00aa3c6f7da012c6f79de610453882
jwilliams108 <jwilliams108@hotmail.com>
2016-07-21 18:39:40 +1000

    initial commit of karmaflair bot

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+Various bots for reddit.
+
 # FlairSync
 A python script/bot for [reddit](http://www.reddit.com) to keep flair in sync across related subreddits
 
@@ -5,20 +7,11 @@ A python script/bot for [reddit](http://www.reddit.com) to keep flair in sync ac
 
 FlairSync requires [PRAW](http://praw.readthedocs.org/en/latest/index.html), and also requires that you have correctly setup [OAuth access](https://github.com/reddit/reddit/wiki/OAuth2).
 
-# Getting Started
+# KarmaFlair
+A python script/bot for [reddit](http://www.reddit.com) to award user granted karma
 
-To begin, create a flairsync.ini file from the sample provided, modifying fields as necessary - setup a list of your subreddits, as well as user and application credentials, and specify which flair(s) are considered valid (i.e., which to sync) via a regular expression.
+## Requirements
 
-You may choose between continuous or single mode - the former will keep the script running, executing the sync at an interval specified by loop_time (in seconds).
+KarmaFlair requires [PRAW](http://praw.readthedocs.org/en/latest/index.html), and also requires that you have correctly setup [OAuth access](https://github.com/reddit/reddit/wiki/OAuth2).
 
-You may also choose between automatic or manual operation - automatic will determine which flair to choose based on the longest length, while manual will prompt the user for a selection.
-
-In general, continuous and automatic mode/operation are meant to work together as a bot, while single and manual mode/operation are meant to function as a script.
-
-Output verbosity can be changed by altering the debug level to DEBUG from NOTICE for more information on the actions being taken by the flairsync script.
-
-## What's New
-
-New version now supports syncing across more than two subs.
-
-A note about automatic mode - it is based on flair length as this script is used across a set of subs that share platinum trophy flair - one for each game. Therefore, the longest valid flair string contains the most trophies and is considered the preferred flair.
+KarmaFlair also requires [psycopg2](http://initd.org/psycopg/) and a PostgreSQL database (see karmaflair.sql for DB schema).
---
+++ karmaflair.py
@@ -0,0 +1,187 @@
+#!/usr/bin/env python
+# vim: ts=4 sts=4 et sw=4
+
+# A script/bot for reddit to manage user awarded karma
+#
+# see karmaflair.ini to set options
+
+from praw import Reddit
+from praw import helpers
+from reddit import reddit_auth
+import ConfigParser
+import psycopg2
+import sys
+import time
+from datetime import datetime
+import re
+from pprint import pprint
+
+# globals
+debug_level = ''
+cfg_file = None
+r = None
+conn = None
+cur = None
+
+
+# helper class for tee logging
+class FlushOutput(object):
+    def __init__(self):
+        self.terminal = sys.stdout
+
+    def write(self, message):
+        self.terminal.write(message)
+        self.terminal.flush()
+
+# sys.stdout = FlushOutput()
+
+
+def set_karma_flair(name):
+    try:
+        # get their total karma from the db
+        cur.execute("SELECT name, count(*) AS karma FROM karma WHERE name=%s GROUP BY name", (name,))
+        result = cur.fetchone()
+
+        if result[1]:
+            # grab their existing flair info
+            subreddit = cfg_file.get('karmaflair', 'subreddit')
+            current_flair = r.get_flair(subreddit, name)
+            karma_flair_text = str(result[1]) + " Karma"
+
+            r.set_flair(subreddit, name, karma_flair_text, current_flair['flair_css_class'])
+    except Exception as e:
+        conn.rollback()
+
+        sys.stderr.write('[{}] [ERROR]: {}'.format(datetime.now().strftime('%Y-%m-%d %H:%M:%S'), e))
+    else:
+        conn.commit()
+
+        if debug_level == 'NOTICE' or debug_level == 'DEBUG':
+            print('[{}] [NOTICE] Karma flair successfully updated for {} to {}'
+                    .format(datetime.now().strftime('%Y-%m-%d %H:%M:%S'), name, karma_flair_text))
+
+
+def process_comment_command(command, command_type, comment, submission, parent=None):
+    if command == 'karma' and command_type == '+' and parent is not None:
+        # valid grant karma command, check for additional criteria
+        while True:
+            # request must have correct link flair
+            if submission.link_flair_text != cfg_file.get('karmaflair', 'valid_flair_text'):
+                break
+
+            # user granting karma must be the same as the submitter, or the parent must be the submitter
+            if comment.author.name != comment.link_author and parent.author.name != comment.link_author :
+                break
+
+            # user cannot grant karma to themselves
+            if parent.author.name == comment.author.name:
+                break
+
+            # cannot grant karma to another command
+            if re.match("^([\+|-])(" + valid_commands + ")$", parent.body.lower().strip())
+                break
+
+            try:
+                cur.execute("INSERT INTO " + cfg_file.get('karmaflair', 'dbtablename') + " (id, name, granter) VALUES (%s, %s, %s)",
+                        (submission.id, parent.author.name, comment.author.name))
+            except psycopg2.IntegrityError as e:
+                conn.rollback()
+
+                if e.pgcode == '23505':
+                    # unique key violation
+                    if debug_level == 'NOTICE' or debug_level == 'DEBUG':
+                        print('[{}] [NOTICE] Karma has already been granted to {} by {}, for submission {}'
+                                .format(datetime.now().strftime('%Y-%m-%d %H:%M:%S'), parent.author.name, comment.author.name, submission.id))
+                else:
+                    sys.stderr.write('[{}] [ERROR]: {}'.format(datetime.now().strftime('%Y-%m-%d %H:%M:%S'), e))
+            else:
+                conn.commit()
+
+                print('[{}] Karma successfully granted to {} by {}, for submission {}'
+                        .format(datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+                        parent.author.name, comment.author.name, submission.id))
+
+                # update karma flair
+                set_karma_flair(parent.author.name)
+
+            break
+
+
+def main():
+    global cfg_file
+    global debug_level
+    global r
+    global conn
+    global cur
+
+    # read ini and set config
+    cfg_file = ConfigParser.RawConfigParser()
+    while True:
+        try:
+            cfg_file.read('karmaflair.ini')
+            break
+        except Exception as e:
+            sys.stderr.write('[{}] [ERROR]: {}'.format(datetime.now().strftime('%Y-%m-%d %H:%M:%S'), e))
+            sys.exit()
+
+    debug_level = cfg_file.get('debug', 'level')
+    mode = cfg_file.get('general', 'mode')
+    loop_time = cfg_file.getint('general', 'loop_time')
+    subreddit = cfg_file.get('karmaflair', 'subreddit')
+    valid_commands = cfg_file.get('karmaflair', 'valid_commands')
+
+    # main loop at set interval if mode is set to 'continuous'
+    while True:
+        print('[{}] Starting karma flair...'
+              .format(datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
+
+        try:
+            # connect to db
+            conn = psycopg2.connect('dbname=' + cfg_file.get('karmaflair', 'dbname') + ' user=' +
+                    cfg_file.get('karmaflair', 'dbuser'))
+            cur = conn.cursor()
+
+            # login
+            r = Reddit(user_agent=cfg_file.get('auth', 'user_agent'))
+            reddit_auth(r, cfg_file, debug_level)
+
+            # retrieve comments, stream will go back limit # of comments from start
+            for comment in helpers.comment_stream(r, subreddit, limit=100, verbosity=0):
+                if not comment.is_root:
+                    # comment has a parent that isn't the submission, so is a candidate for a command
+                    if debug_level == 'NOTICE' or debug_level == 'DEBUG':
+                        print('[{}] [NOTICE] Checking comment posted at {} by {}'
+                            .format(datetime.now().strftime('%Y-%m-%d %H:%M:%S'), datetime.utcfromtimestamp(comment.created_utc), comment.author.name))
+
+                    # command format is to start with a + or - and be the only text in the comment, whitespace is excluded
+                    match = re.match("^([\+|-])(" + valid_commands + ")$", comment.body.lower().strip())
+
+                    if match is not None and match.group(2):
+                        # comment is both a sub comment and contains a valid command
+                        command = match.group(2)
+                        command_type = match.group(1)
+
+                        submission = r.get_info(thing_id=comment.link_id)
+                        parent = r.get_info(thing_id=comment.parent_id)
+
+                        process_comment_command(command, command_type, comment, submission, parent)
+
+            if mode == 'continuous':
+                print('[{}] Pausing karma flair...'
+                    .format(datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
+                time.sleep(loop_time)
+            else:
+                cur.close()
+                conn.close()
+                break
+        except KeyboardInterrupt, SystemExit:
+            cur.close()
+            conn.close()
+
+            print('[{}] Stopping summon karma...'.format(datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
+            break
+        except Exception as e:
+            sys.stderr.write('[{}] [ERROR]: {}'.format(datetime.now().strftime('%Y-%m-%d %H:%M:%S'), e))
+
+if __name__ == '__main__':
+    main()
